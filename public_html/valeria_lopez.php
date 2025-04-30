761b9a4c14af5da3bb9c0a7e343a6b964d8f43be
Adrian Jacobo <jacobo@Adrians-Mac-mini.local>
2025-04-29 21:41:10 -0600

    Se actualizan las fechas

--- public_html/valeria_lopez.php
+++ public_html/valeria_lopez.php
@@ -88,7 +88,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-12 lemon-milk-light">
-                    <p>&copy; 2024 <a href="http://elquintosolmarketing.com">El quinto sol</a><br>Todos los derechos reservados.</p>
+                    <p>&copy; 2025 <a href="http://elquintosolmarketing.com">El quinto sol</a><br>Todos los derechos reservados.</p>
                     <br>
                     <p>Designed by <a href="http://mx.duck-hack.cloud" target="_blank" rel="noopener noreferrer">Duck-Hack</a></p>
                 </div>
